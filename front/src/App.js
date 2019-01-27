5bdbdc0932479e4c032bd3d398995c15e843f890
marusel12 <marusel.12@gmail.com>
2019-01-27 18:48:38 +0700

    добавлена библиотека компонентов

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello, Alkash!!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <TextField
+            required
+            id="standard-required"
+            label="email"
+            margin="normal"
+          />
+          <TextField
+            required
+            id="standard-required"
+            label="password"
+            margin="normal"
+          />
         </header>
       </div>
     );
