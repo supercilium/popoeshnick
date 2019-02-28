07c4ea3825769056c59d05ec81e286477ab34d6f
Rarnz <lightnekromant@gmail.com>
2019-02-28 18:44:22 +0300

    bla

--- front/src/components/home/home.jsx
+++ front/src/components/home/home.jsx
@@ -1,3 +1,43 @@
 import React from 'react';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
 
-export default () => <h2>Hello Alkash!</h2>;
+
+export default () => (
+  <div>
+    <h2>Hello Alkash!</h2>
+    <PopoykaList />
+  </div>
+);
+
+class PopoykaList extends React.Component {
+  state = {
+    expanded: null,
+  };
+
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+
+  render() {
+    const { expanded } = this.state;
+
+    return (
+      <div>
+        <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+          <ExpansionPanelSummary>
+            <Typography>General settings</Typography>
+            <Typography>I am an expansion panel</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>xui</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    );
+  }
+}
