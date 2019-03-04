f9b16190ed8b32f4abd0008c021210191a573e27
Kirill Shirolapov <kshirolapov@odin.com>
2019-03-04 12:15:31 +0700

    Add migrate to app

--- dummy/Popoeshnick/app.py
+++ dummy/Popoeshnick/app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import Flask
+from flask_migrate import Migrate
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -10,6 +11,8 @@ from flask import jsonify
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 
 class User(db.Model):
