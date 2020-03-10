c909dfcd8e7bdf86eb60ea45a5fb3accd13aa7ba
Kirill Shirolapov <kirill.shirolapov@gmail.com>
2020-03-11 00:26:46 +0700

    Worked init file for vagrant

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y nodejs
+sudo apt install nodejs postgresql postgresql-contrib python3-pip virtualenv nginx -y
 sudo npm install -g react-scripts
 
 mkdir ~/vagrant_node_modules
@@ -12,31 +12,27 @@ cd /vagrant_data/front/
 npm i
 npm run build
 
-sudo apt install nginx -y
-sudo ln -s /vagrant_data/confs/site.conf /etc/nginx/sites-enabled/
-sudo nginx -s reload
-
-sudo apt install -y postgresql postgresql-contrib
-sudo apt install -y python3-pip
-sudo apt install -y virtualenv
-
 virtualenv -p python3 /home/vagrant/popo/
 /home/vagrant/popo/bin/pip install -r /vagrant_data/dummy/requirements.txt
 
-sudo su
-sudo echo "127.0.0.1 db" >> /etc/hosts
-exit
-
+sudo cp /vagrant_data/confs/site.conf /etc/nginx/sites-enabled/
+sudo cp /vagrant_data/confs/hosts /etc/hosts
 sudo cp /vagrant_data/confs/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
 sudo cp /vagrant_data/confs/postgresql.conf /etc/postgresql/10/main/postgresql.conf
 
-sudo -i -u postgres
-createuser popo
-psql -c "ALTER USER popo WITH PASSWORD '123'"
-psql -c "CREATE DATABASE popo OWNER popo"
-exit
+sudo -u postgres createuser popo
+sudo -u postgres psql -c "ALTER USER popo WITH PASSWORD '123'"
+sudo -u postgres psql -c "CREATE DATABASE popo OWNER popo"
 
+sudo systemctl restart nginx
 sudo systemctl restart postgresql
 
-/home/vagrant/popo/bin/python /vagrant_data/dummy/Popoeshnick/manage.py db init
-/home/vagrant/popo/bin/python /vagrant_data/dummy/Popoeshnick/manage.py db migrate
+rm -rf /vagrant_data/dummy/Popoeshnick/migrations/
+
+/home/vagrant/popo/bin/python /vagrant_data/dummy/Popoeshnick/manage.py db init -d /vagrant_data/dummy/Popoeshnick/migrations/
+/home/vagrant/popo/bin/python /vagrant_data/dummy/Popoeshnick/manage.py db migrate -d /vagrant_data/dummy/Popoeshnick/migrations/
+/home/vagrant/popo/bin/python /vagrant_data/dummy/Popoeshnick/manage.py db upgrade -d /vagrant_data/dummy/Popoeshnick/migrations/
+
+mkdir -p /var/log/uwsgi/
+sudo chown vagrant /var/log/uwsgi/
+/home/vagrant/popo/bin/uwsgi --ini /vagrant_data/dummy/Popoeshnick/wsgi.ini
