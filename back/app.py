9f17a6962c6bef67750f0e0cf2f70086826c9727
unknown <1374688@gmail.com>
2020-07-10 17:01:32 +0800

    basic sync with front - requests are received

--- back/app.py
+++ back/app.py
@@ -1,29 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-users = [
-        {'name': 'John',
-         'score': 12,
-         'budget': 1400},
-        {'name': 'Bob',
-         'score': 29,
-         'budget': 2800},
-        ]
-
 
 class User(Resource):
-    def get(self, name):
-        for user in users:
-            if user['name'].lower() == name.lower():
-                return user
-        return {'name': None}, 404
+# 'registration' - receiving request from front
+    def post(self):
+        new_user = request.get_json()
+        print ('got this:',new_user)
 
-
-api.add_resource(User, '/user/<string:name>')
+api.add_resource(User, '/api/user/registration/')
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port=3000)
