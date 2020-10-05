e2098c12263d4082868e7c1c9da44c20bbba922f
Marusel <marusel@EvilMac.local>
2020-10-05 22:52:27 +0300

    added Formik to Login Form

--- front/src/components/loginForm/loginForm.jsx
+++ front/src/components/loginForm/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import {
   TextField,
   Button,
@@ -9,6 +9,7 @@ import red from '@material-ui/core/colors/red'
 import PropTypes, { any } from 'prop-types'
 import _ from 'lodash'
 import axios from 'axios'
+import { Formik } from 'formik'
 
 import { validateEmail, validatePass } from '../../utils'
 import { API_CONST } from '../../constants'
@@ -46,144 +47,109 @@ const styles = {
   },
 }
 
-export class LoginForm extends PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: null,
-      password: null,
-      errors: {
-        email: [],
-        password: [],
-        login: [],
-      },
-    }
-  }
+export const LoginForm = ({ onLogin, classes }) => {
 
-  handleSetPassword = (event) => {
-    const errMsg = ['must contain at list 8 charecters']
-    this.setState({
-      password: event.target.value,
-      errors: {
-        password: validatePass(event.target.value) ? [] : errMsg,
-      },
-    })
-  }
-
-  handleSetLogin = (event) => {
-    const errMsg = ['invailid email address']
-    this.setState({
-      email: event.target.value,
-      errors: {
-        email: validateEmail(event.target.value) ? [] : errMsg,
-      },
-    })
-  }
-
-  handleLogin = () => {
-    const {
-      email,
-      password,
-    } = this.state
-    const {
-      onLogin,
-    } = this.props
+  const handleLogin = (values, setErrors) => {
     axios.post(API_CONST.LOGIN, {
-      password,
-      email,
+      password: values.password,
+      email: values.email,
     }).then((res) => {
       const { errors, profile, status } = res.data
       if (status === 'success') {
         onLogin(profile)
       } else {
-        this.setState({
-          errors: {
-            login: errors.login,
-            email: errors.email,
-            password: errors.password,
-          },
-        })
+        const errorObj = {
+          email: errors?.email?.reduce((acc, str) => `${str} ${acc}`, ''),
+          password: errors?.password?.reduce((acc, str) => `${str} ${acc}`, '')
+        }
+        setErrors(errorObj)
       }
     }).catch((error) => {
+      // TODO Form error
       console.log(error)
     })
   }
 
-  handleSubmit = (e) => {
-    const {
-      errors,
-    } = this.state
-    const passError = !_.isEmpty(errors.password)
-    const emailError = !_.isEmpty(errors.email)
-    if (!emailError && !passError) {
-      this.handleLogin()
-    }
-    e.preventDefault()
-  }
-
-  renderError = (msgs) => {
-    const { classes } = this.props
-    return msgs && msgs.map(msg => <Typography key={msg} classes={{ root: classes.error }} variant="body2">{msg}</Typography>)
-  }
-
-  render() {
-    const {
-      password,
-      email,
-      errors,
-    } = this.state
-    const { classes } = this.props
-    const passError = !_.isEmpty(errors.password)
-    const emailError = !_.isEmpty(errors.email)
     return (
-      <form
-        id="loginForm"
-        onSubmit={this.handleSubmit}
-        onChange={this.handleFormChange}
-        className={classes.form}
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          // TODO move to utils
+          const errors = {}
+          if (!values.email) {
+            errors.email = 'Required'
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address'
+          }
+          if (!values.password) {
+            errors.password = 'Required'
+          } else if (!validatePass(values.password)) {
+            errors.password = 'Password must be at least 8 characters long'
+          }
+          return errors
+        }}
+        onSubmit = { async (values, { setSubmitting, setErrors }) => {
+          handleLogin(values, setErrors)
+          setSubmitting(false)
+        }}
       >
-        <Typography component="h1" variant="h4" classes={{ root: classes.title }}>
-          Hello, Alkash!!!
-        </Typography>
-        <Typography component="h2" variant="subtitle1" classes={{ root: classes.subtitle }}>
-          Explore alco-possibilities with new awesome service Popoeshnick
-        </Typography>
-        <TextField
-          required
-          label="email"
-          margin="dense"
-          type="email"
-          className={classes.input}
-          onChange={this.handleSetLogin}
-          error={emailError}
-          helperText={errors.email}
-          autoComplete="username"
-        />
-        <TextField
-          required
-          label="password"
-          margin="dense"
-          type="password"
-          className={classes.input}
-          onChange={this.handleSetPassword}
-          error={passError}
-          helperText={errors.password}
-          autoComplete="current-password"
-        />
-        {!_.isEmpty(errors.login) && this.renderError(errors.login)}
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          classes={{ root: classes.button }}
-          // onClick={this.handleLogin}
-          disabled={!email || !password || passError || emailError || !_.isEmpty(errors.loginError)}
-        >
-          Log in
-        </Button>
-      </form>
+        {({
+         values,
+         errors,
+         handleChange,
+         handleSubmit,
+         isSubmitting,
+       }) => (
+          <form
+            id="loginForm"
+            onSubmit={handleSubmit}
+            className={classes.form}
+          >
+            <Typography component="h1" variant="h4" classes={{ root: classes.title }}>
+              Hello, Alkash!!!
+            </Typography>
+            <Typography component="h2" variant="subtitle1" classes={{ root: classes.subtitle }}>
+              Explore alco-possibilities with new awesome service Popoeshnick
+            </Typography>
+            <TextField
+              required
+              label="email"
+              margin="dense"
+              type="email"
+              name="email"
+              className={classes.input}
+              onChange={handleChange}
+              error={errors.email}
+              helperText={errors.email}
+              autoComplete="username"
+            />
+            <TextField
+              required
+              label="password"
+              margin="dense"
+              type="password"
+              name="password"
+              className={classes.input}
+              onChange={handleChange}
+              error={errors.password}
+              helperText={errors.password}
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              classes={{ root: classes.button }}
+              disabled={!values.email || !values.password || !_.isEmpty(errors.loginError) || isSubmitting}
+            >
+              Log in
+            </Button>
+          </form>
+       )}
+      </Formik>
     )
-  }
 }
 
 LoginForm.propTypes = {
